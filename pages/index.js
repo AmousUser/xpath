275694b6d26357763fa7efabad6f02a45c518e7f
Sergiy Ostrovsky <sergiy.ostrovsky95@gmail.com>
2022-03-25 17:44:49 +0200

    master some-changes123

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Box, } from '../ui';
 //////////////////////////////////////////////////
 
 const HomePage = () => (
-  <Box fontSize={{ _: 9, sm: 24 }}>Say Hello</Box>
+  <Box fontSize={{ _: 9, sm: 24 }}>Some</Box>
 );
 
 export default HomePage;
